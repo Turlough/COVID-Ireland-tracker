482877f0477b794068afbeca0f15929d0e1d101d
Turlough <turloughcowman@gmail.com>
2020-03-22 23:55:18 +0000

    Cleanup and comments

--- scraper.py
+++ scraper.py
@@ -6,28 +6,26 @@ import matplotlib.pyplot as plt
 
 url = 'https://www.worldometers.info/coronavirus/country/ireland/'
 content = requests.get(url).text
-
 soup = BeautifulSoup(content, features='html.parser')
+
+# Find the script element following div with id 'graph-cases-daily'
 script = soup.find('div', id='graph-cases-daily').findNext('script')
-# print(script.text)
+# Find the data line
 lines = script.text.split('\n')
 datas = [l for l in lines if l.strip().startswith('data')]
-data = datas[0].strip()
-# print(data)
-
-counts_text = re.findall('[0-9]+', data)
+data_line = datas[0].strip()
+# Extract the data from the data_line
+counts_text = re.findall('[0-9]+', data_line)
 counts = list(map(int, counts_text))
-print(counts)
-
+# Create DataFrame with Cumulative Sum of the daily data
 df = pd.DataFrame(counts, columns=['DailyIncrease'])
 df['Total'] = df.DailyIncrease.cumsum()
+
+# Display the data
 print(df)
-
-di = df.DailyIncrease
-tot = df.Total
-
-plt.plot(di, label = 'Daily Increase')
-plt.plot(tot, label = 'Total Cases')
+# Plot the data
+plt.plot(df.DailyIncrease, label = 'Daily Increase')
+plt.plot(df.Total, label = 'Total Cases')
 plt.title('Ireland')
 plt.legend()
 plt.grid()
